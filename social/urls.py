c3d5f370f2a8e3ebde5ba78cb2868a03176ccfb2
Anjali Ayer <anjali80@gmail.com>
2026-07-06 17:49:48 +0545

    hello

--- social/urls.py
+++ social/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
@@ -14,10 +15,13 @@ urlpatterns = [
     path('post/<int:post_id>/like/', views.like_post_view, name='like_post'),
     path('post/<int:post_id>/comment/', views.comment_post_view, name='comment_post'),
 
+    path('comment/<int:comment_id>/delete/', views.delete_comment_view, name='delete_comment'),
+
     path('notifications/', views.notifications_view, name='notifications'),
 
     path('profile/<int:user_id>/', views.profile_view, name='profile'),
     path('edit-profile/', views.edit_profile, name='edit_profile'),
+
     path('search/', views.search_user, name='search'),
     path('follow/<int:user_id>/', views.follow_user, name='follow'),
 ]
